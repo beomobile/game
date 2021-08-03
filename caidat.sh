84851b4002d171588b7a1a9dd60713a42d049d4e
beomobile <60918933+beomobile@users.noreply.github.com>
2021-08-03 15:07:55 +0700

    Update caidat.sh

--- caidat.sh
+++ caidat.sh
@@ -7,7 +7,7 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip 
 clear
 read -p "Nhập mã ngrok authtoken của bạn vào đây (Sao chép và nhấp chuột phải để dán vào đây): " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp --region asia 30889 &>/dev/null &
+nohup ./ngrok tcp --region us 30889 &>/dev/null &
 yum install sudo -y
 echo "Downloading QEMU ..."
 sudo yum install -y qemu-kvm
